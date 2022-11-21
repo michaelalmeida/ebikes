087eda447d53fa280063677f00579d06eb981220
Michael Almeida <michaelralmeida@gmail.com>
2022-11-21 20:49:22 +0100

    feat: initial commit for client

---
+++ client/src/Constants/colors.ts
@@ -0,0 +1,5 @@
+export const BLACK = '#424242';
+export const MAIN_COLOR = '#26C6DA';
+export const LIGHTER_MAIN_COLOR = '#B2EBF2';
+export const SECONDARY_COLOR = '#F06292';
+export const WHITE = '#f5f5f5';
---
+++ client/src/GlobalStyles.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components';
+import { BLACK, MAIN_COLOR, WHITE } from './Constants/colors';
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    color: ${BLACK};
+    background: ${WHITE};
+    font-family: 'Inter', sans-serif;
+  }
+
+  a {
+    color: ${MAIN_COLOR};
+    text-decoration: none
+  }
+
+  button {
+    border: 4px solid ${MAIN_COLOR};
+    background: transparent;
+    padding: 5px 20px;
+    text-transform: uppercase;
+    font-weight: 900;
+    font-size: 14px;
+    cursor: pointer;
+    @media screen and (max-width: 800px) {
+      padding: 5px;
+  }
+}
+`;
+
+export default GlobalStyle;
